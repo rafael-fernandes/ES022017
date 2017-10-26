3d8e439606d2a929032303cc5836c5c9b1c0012d
igorsodre <igorcuei@yahoo.com.br>
2017-10-26 15:20:32 -0200

    css tela login + assests adicionados ao repo

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -2,6 +2,10 @@ class SeriesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @series = Serie.all
+    if params[:filtro]
+      @series = Serie.where('title like (?)', ["%#{params[:filtro]}%"])
+    else
+      @series = Serie.all
+    end
   end
 end
