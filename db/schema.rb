3c4699dae211c4f59ac1974b169dc4fdc3b8d920
igorsodre <igorcuei@yahoo.com.br>
2017-10-26 18:04:04 -0200

    trocando de mysql para postrgres por conta do heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,15 +12,18 @@
 
 ActiveRecord::Schema.define(version: 20171026184639) do
 
-  create_table "series", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "series", force: :cascade do |t|
     t.string   "title"
-    t.text     "description", limit: 65535
+    t.text     "description"
     t.string   "year"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
