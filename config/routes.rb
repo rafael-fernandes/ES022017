b58d75a4383f5831e24ea3f0ddb4f79bb539df7c
igorsodre <igorcuei@yahoo.com.br>
2017-10-26 17:19:25 -0200

    tela de cadastro finalizada + assets inseridos no repo

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'series#index'
 
   resources :series
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: "users/registrations"
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
