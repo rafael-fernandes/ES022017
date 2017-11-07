aac9bb0ee45f668c3ab81e7e1d33ff38b1bdcbbf
amandaazeitona <amandaalves@cjr.org.br>
2017-11-07 16:39:21 -0200

    paginacao com kaminari

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -3,9 +3,9 @@ class SeriesController < ApplicationController
 
   def index
     if params[:filtro]
-      @series = Serie.where('title like (?)', ["%#{params[:filtro]}%"])
+      @series = Serie.where('title like (?)', ["%#{params[:filtro]}%"]).page(:page).per(16)
     else
-      @series = Serie.all
+      @series = Serie.all.page(:page).per(16)
     end
   end
 end
