507c1c843aeca95a0b7ac2923645d4881c087d00
Frederico Dib <fredericodib@cjr.org.br>
2017-11-12 18:52:44 -0200

    front arrumado e filtro funcionando

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -2,9 +2,9 @@ class SeriesController < ApplicationController
 
   def index
     if params[:filtro]
-      @series = Serie.where('title like (?)', ["%#{params[:filtro]}%"]).page(params[:page]).per(16)
+      @series = Serie.where('title like (?)', ["%#{params[:filtro]}%"]).page(params[:page]).per(12)
     else
-      @series = Serie.all.page(params[:page]).per(16)
+      @series = Serie.all.page(params[:page]).per(12)
     end
   end
 end
