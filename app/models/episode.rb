117684d55efd2c909c1d6050663cc9bc22095a7b
Rafael Fernandes <spnrafael@gmail.com>
2017-11-26 18:41:50 -0200

    Teste unitário de episódio

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,3 +1,7 @@
 class Episode < ApplicationRecord
   belongs_to :season
+
+  validates :name, presence: true
+  validates :number, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :release_date, presence: true
 end
