3e9dc40a83f54105ce983330603c39f111ad8f40
Rafael Fernandes <spnrafael@gmail.com>
2017-11-26 19:49:24 -0200

    Atualiza nomes e atributos de models

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,9 @@ Rails.application.routes.draw do
   get '/:page' => 'series#index'
 
   resources :series
+  
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: "users/registrations"
   }
-
-  get '/add_list/:serie_id' => 'series_acompanhadas#add_to_list', as: :add_list_serie
-  get '/delete_list/:serie_id' => 'series_acompanhadas#remove_from_list', as: :delete_list_serie
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
